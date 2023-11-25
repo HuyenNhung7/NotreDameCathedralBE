34a3555ad05ecb911b3f22316cd684e7544501a5
LAPTOP-TRUNG <thanhtrungvnger@gmail.com>
2023-11-25 14:27:56 +0700

    add field name in table feedback

--- src/models/feedback.js
+++ src/models/feedback.js
@@ -1,21 +1,26 @@
 import mongoose from "mongoose";
 
-const feedbackSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-    },
-    sdt: {
-        type: String,
-    },
-    message: {
-        type: String,
-    },
-},
+const feedbackSchema = new mongoose.Schema(
     {
-        timestamps: true
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        sdt: {
+            type: String,
+        },
+        message: {
+            type: String,
+        },
+    },
+    {
+        timestamps: true,
     }
 );
 
-const Feedback = mongoose.model('feedback', feedbackSchema)
-export default Feedback; 
+const Feedback = mongoose.model("feedback", feedbackSchema);
+export default Feedback;
